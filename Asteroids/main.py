cd70e507c2e79f4887d6fc6846b73e82a4542e3d
Sanguine-Lord <brscarter@gmail.com>
2025-08-11 13:02:30 +0100

    added astroid field logic and asteroid object

--- Asteroids/main.py
+++ Asteroids/main.py
@@ -1,20 +1,32 @@
 import pygame
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
     pygame.init()
     gameclock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player((SCREEN_WIDTH/2),(SCREEN_HEIGHT/2))
+    asteroidfield = AsteroidField()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0,0,0))
-        player.update(dt)
-        player.draw(screen)
+        for object in updatable:
+            object.update(dt)
+        for d_object in drawable:
+            d_object.draw(screen)
         pygame.display.flip()
         dt = gameclock.tick(60) / 1000        
 
--- Asteroids/player.py
+++ Asteroids/player.py
@@ -2,7 +2,6 @@ from circleshape import *
 from constants import *
 import pygame
 
-
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
@@ -18,18 +17,23 @@ class Player(CircleShape):
         return [a, b, c]
 
     def draw(self, screen):
-        print(self.triangle())
         playershape = self.triangle()
         pygame.draw.polygon(screen,(255,255,255),playershape,2)
 
     def rotate(self,dt):
         self.rotation += PLAYER_TURN_SPEED * dt
-    
+        
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
     def update(self, dt):
         keys = pygame.key.get_pressed()
-        print("A pressed:", keys[pygame.K_a], "D pressed:", keys[pygame.K_d])
         if keys[pygame.K_a]:
             self.rotate(-dt)
         if keys[pygame.K_d]:
             self.rotate(dt)
-
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
